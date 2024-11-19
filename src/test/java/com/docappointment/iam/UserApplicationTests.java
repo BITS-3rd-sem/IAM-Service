c6af8d3d0c5460fd73375a7b02012df430947f45
snehasish-20 <snehasish.pati@sap.com>
2024-11-20 00:55:53 +0530

    initial commit

---
+++ src/test/java/com/docappointment/iam/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docappointment.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
